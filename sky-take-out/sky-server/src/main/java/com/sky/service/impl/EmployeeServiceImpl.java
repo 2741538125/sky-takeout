39a5cfb289628eae764de5217853609759a94f2b
2741538125 <2741538125@qq.com>
2024-07-20 22:38:03 +0800

    员工分页查询代码开发

--- sky-take-out/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,21 +1,26 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.constant.MessageConstant;
 import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
 import com.sky.context.BaseContext;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
 import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
+import com.sky.result.PageResult;
 import com.sky.service.EmployeeService;
 
 import ch.qos.logback.core.joran.util.beans.BeanUtil;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -92,5 +97,23 @@ public class EmployeeServiceImpl implements EmployeeService {
         employee.setUpdateUser(BaseContext.getCurrentId());
 
         employeeMapper.insert(employee);
+    }
+
+    /**
+     * 分页查询
+     * @param enEmployeePageQueryDTO
+     * @return
+     */
+    public PageResult pageQuery(EmployeePageQueryDTO enEmployeePageQueryDTO) {
+        // select * from employee limit 0, 10
+        //开始分页查询
+        PageHelper.startPage(enEmployeePageQueryDTO.getPage(), enEmployeePageQueryDTO.getPageSize());
+
+        Page<Employee> page = employeeMapper.pageQuery(enEmployeePageQueryDTO);
+
+        long total = page.getTotal();
+        List<Employee> records = page.getResult();
+
+        return new PageResult(total, records);
     }
 }
