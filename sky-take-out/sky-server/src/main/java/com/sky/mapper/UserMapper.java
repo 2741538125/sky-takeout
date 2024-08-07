a2b5a052539a2c4dc6b496eb95a2cd41a59dc0dd
2741538125 <2741538125@qq.com>
2024-08-07 21:05:18 +0800

    跳过微信付款的无付款订单支付代码开发

--- sky-take-out/sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-take-out/sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -22,4 +22,13 @@ public interface UserMapper {
      */
     void insert(User user);
 
+
+    /**
+     * 根据用户id查询用户信息
+     * @param userId
+     * @return
+     */
+    @Select("select * from user where id = #{userId}")
+    User getById(Long userId);
+
 }
